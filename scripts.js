314447aec1a652ee2218cdd4fc58e787ab6a74aa
lcordo <lc2802@hotmail.com>
2025-01-27 21:52:15 -0300

    Se mejoraron los estilos y terminaron funcionalidades de los mapas, entrega lista

--- scripts.js
+++ scripts.js
@@ -1,30 +1,91 @@
-const capabilitiesUrl = 'https://wms.ign.gob.ar/geoserver/gwc/service/wmts?request=GetCapabilities';
+const wms1 = "https://wms.ign.gob.ar/geoserver/ows";
+const wms2 = "https://imagenes.ign.gob.ar/geoserver/coberturas_del_suelo/ows";
+const wms3 = "https://wms.ign.gob.ar/geoserver/ign_riesgo/ows"
 
-fetch(capabilitiesUrl)
-  .then(response => response.text())
-  .then((capabilitiesText) => {
-    const parser = new ol.format.WMTSCapabilities();
-    const capabilities = parser.read(capabilitiesText);
+let mapaBaseHTML = document.getElementById("mapa");
+let capaComplementariaHTML = document.querySelectorAll("#capacomp1 input[type='checkbox']");
+let map;
 
-    const options = ol.source.WMTS.optionsFromCapabilities(capabilities, {
-      layer: 'mapabase_gris', 
-      matrixSet: 'EPSG:3857', 
-    });
+function crearMapa(mapa, capasSeleccionadas) {
+  if (map) {
+    map.setTarget(null);
+    map = null; 
+  }
 
-   
-    const wmtsLayer = new ol.layer.Tile({
-      source: new ol.source.WMTS(options),
-    });
+  const LAYERS = [];
+console.log(mapa)
+  const wmsLayerBase = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url: wms1,
+      params: {
+        LAYERS: mapa, 
+        FORMAT: "image/png",
+        TILED: true, 
+        TRANSPARENT: true,
+      },
+      ratio: 1,
+      serverType: "geoserver",
+    }),
+  });
+  LAYERS.push(wmsLayerBase);
 
-    const map = new ol.Map({
-      target: 'map',
-      layers: [wmtsLayer],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([-60.7077, -31.6233]), 
-        zoom: 14,
+  capasSeleccionadas.forEach((capa) => {
+
+    let wmsCapaComplementaria;
+
+    if (capa === "coberturas_del_suelo") {
+      wmsCapaComplementaria = wms2;
+    }
+
+    else{
+
+      wmsCapaComplementaria=wms3;
+
+    }
+    
+    const layer = new ol.layer.Tile({
+      source: new ol.source.TileWMS({
+        url: wmsCapaComplementaria,
+        params: {
+          LAYERS: capa, 
+          FORMAT: "image/png",
+          TRANSPARENT: true,
+        },
+        ratio: 1,
+        serverType: "geoserver",
       }),
     });
-  })
-  .catch((error) => {
-    console.error('Error al cargar el archivo de capacidades WMTS:', error);
+    LAYERS.push(layer);
   });
+
+  map = new ol.Map({
+    target: "map",
+    layers: LAYERS,
+    view: new ol.View({
+      center: ol.proj.fromLonLat([-64, -38]), 
+      zoom: 5,
+    }),
+    controls: [] // Esto desactiva todos los controles
+
+  });
+}
+
+mapaBaseHTML.addEventListener("change", function () {
+  const mapa = mapaBaseHTML.value; 
+  const capasSeleccionadas = Array.from(capaComplementariaHTML)
+    .filter((checkbox) => checkbox.checked) 
+    .map((checkbox) => checkbox.value); 
+
+  crearMapa(mapa, capasSeleccionadas); 
+});
+
+capaComplementariaHTML.forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    const mapa = mapaBaseHTML.value; 
+    const capasSeleccionadas = Array.from(capaComplementariaHTML)
+      .filter((checkbox) => checkbox.checked) 
+      .map((checkbox) => checkbox.value); 
+
+    crearMapa(mapa, capasSeleccionadas); 
+  });
+});
